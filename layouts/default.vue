98fe3b5d57679c047f3bfd2ba1bc6353ba7167dd
werlesson <werlessono@gmail.com>
2021-05-26 16:19:12 -0300

    feat(layout): structure layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,57 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="center example-nav">
+    <vs-navbar
+      v-model="active"
+      target-scroll="#padding-scroll-content"
+      padding-scroll
+      center-collapsed
+      square
+    >
+      <template #left> LOGO </template>
+      <vs-navbar-item id="guide" :active="active == 'guide'">
+        Início
+      </vs-navbar-item>
+      <vs-navbar-item id="docs" :active="active == 'docs'">
+        Sobre
+      </vs-navbar-item>
+      <vs-navbar-item id="components" :active="active == 'components'">
+        Projetos
+      </vs-navbar-item>
+      <template #right>
+        <vs-button>Contate-me</vs-button>
+      </template>
+    </vs-navbar>
+    <div id="padding-scroll-content" class="square">
+      <Nuxt />
+    </div>
   </div>
 </template>
-
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+export default {
+  data: () => ({
+    active: 'guide',
+  }),
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+</script>
+<style scoped>
+.square {
+  max-height: 100vh;
+  width: 100%;
+  position: relative;
+  overflow: auto;
+  padding-top: 7rem;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.example-nav {
+  background: #f0f0f0 !important;
+  padding: 0 !important;
+  position: relative;
+  overflow: hidden;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  flex-wrap: wrap;
 }
 </style>
---
+++ views/sections/SectionMain.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>MAIN</main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+main {
+  height: 100vh;
+  background-color: red;
+}
+</style>
